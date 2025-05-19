4a0f3fe71065967a362c277a99173543ad62abb4
Awaneesh Soni <awaneeshsoniofficial@gmail.com>
2025-05-19 16:31:28 +0530

    app.jsx imports fixed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,8 +5,8 @@ import {
   Routes,
 } from "react-router-dom";
 import Home from "./pages/Home";
-import Login from "./pages/login";
-import Signup from "./pages/signup";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
 import Messages from "./pages/Messages";
 import PublicPage from "./pages/PublicPage";
